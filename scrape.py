b9b45270ab15040d9c885cdaa6302cf0c2200567
Harsh Gupta <harsh@berkeley.edu>
2019-09-17 21:11:14 -0700

    fetches rows with all text

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import pickle
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 driver = webdriver.Chrome('/Users/harsh/chromedriver')
@@ -30,9 +31,14 @@ def scrape_website(source_url):
         '/html/body/app-root/offer-search-component/div[2]/offer-summary-popup/div/div/div[2]/div[1]/button').click()
 
     # Main table data scrape now
-    elements = driver.execute_script("document.getElementsByClassName('row dataRow')")
-    for element in elements:
-        print(element)
+    for x in range(3, 150):
+        try:
+            element = driver.find_element_by_xpath(f"/html/body/app-root/offer-search-component/div[2]/div[4]/main/div/div/div[{x}]")
+            print(element.text)
+        except NoSuchElementException:
+            break
+
+
 
     # data_rows = driver.find_elements_by_class_name('row dataRow')
     # print("Data Rows:", data_rows)
@@ -43,4 +49,4 @@ def scrape_website(source_url):
 source_url = "http://documents.dps.ny.gov/PTC/zipcode/10001"
 scrape_website(source_url=source_url)
 
-# //*[@id="offer284156"]
+# /html/body/app-root/offer-search-component/div[2]/div[4]/main/div/div/div[4]
