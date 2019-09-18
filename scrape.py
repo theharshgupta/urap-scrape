da8b604fdadd4312ea2947766ae17e3e907b3f61
Harsh Gupta <31355653+theharshgupta@users.noreply.github.com>
2019-09-17 20:59:05 -0700

    removes the popups

--- scrape.py
+++ scrape.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
 import pickle
+import time
 
-driver = webdriver.Chrome('C:/chromedriver.exe')
+driver = webdriver.Chrome('/Users/harsh/chromedriver')
+
+
+def sleep_one():
+    time.sleep(1)
 
 
 def scrape_website(source_url):
@@ -10,11 +15,32 @@ def scrape_website(source_url):
     website scrape
     :return:
     """
+    # Below part accepts the agreement popup using just the clicks
     driver.get(source_url)
-    driver.implicitly_wait(3)
-    popup = driver.find_element_by_xpath('/html/body/app-root/ptc-overlay/div/div')
-    driver.execute_script("arguments[0].scrollIntoView();", popup)
+    time.sleep(3)
+    driver.find_element_by_xpath('//*[@id="acceptOverlay"]').click()
+    sleep_one()
+    driver.find_element_by_xpath('//*[@id="scrollDown"]').click()
+    sleep_one()
+    driver.find_element_by_xpath('//*[@id="acceptOverlay"]').click()
+
+    # Now we click the view electric popup, fuck these popups I swear
+    sleep_one()
+    driver.find_element_by_xpath(
+        '/html/body/app-root/offer-search-component/div[2]/offer-summary-popup/div/div/div[2]/div[1]/button').click()
+
+    # Main table data scrape now
+    elements = driver.execute_script("document.getElementsByClassName('row dataRow')")
+    for element in elements:
+        print(element)
+
+    # data_rows = driver.find_elements_by_class_name('row dataRow')
+    # print("Data Rows:", data_rows)
+    # for element in data_rows:
+    #     print("Element:", element)
 
 
 source_url = "http://documents.dps.ny.gov/PTC/zipcode/10001"
 scrape_website(source_url=source_url)
+
+# //*[@id="offer284156"]
