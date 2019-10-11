3e6d763c6bcf5bca2c7cafd26ceebb2f01c503fd
Michael Ganzorig <manlai1025@gmail.com>
2019-10-11 13:49:50 -0700

    using pdfkit to download pdfs that dont otherwise download

--- PowerToChoose/scrapeHelper.py
+++ PowerToChoose/scrapeHelper.py
@@ -1,4 +1,5 @@
 import requests, sys, json
+import time, os, pdfkit
 from pdfReader import isPDFFile
 from bs4 import BeautifulSoup
 
@@ -27,14 +28,57 @@ def getJsonFromZIP(zip_code):
     response = getResponseText(zip_code)
     return json.loads(response)
 
-def downloadPDF(link, fileName, folderName):
+def isFileExists(folder_name, file_name_with_extension):
+    return os.path.exists(folder_name + file_name_with_extension)
+
+def getUniquePath(folder_name, preferred_file_name):
+    """
+    initial is the name of the file without the extension
+    """
+    if not isFileExists(folder_name, preferred_file_name + ".pdf"):
+        return folder_name + preferred_file_name + ".pdf"
+    count = 1
+    while(isFileExists(folder_name, preferred_file_name + "-" + str(count) + ".pdf")):
+        count += 1
+    return folder_name + preferred_file_name + "-" + str(count) + ".pdf"
+    
+
+def downloadPDF(link, preferred_file_name, folderName):
     """
     given an url to a pdf file on a webpage,
     download the pdf file locally
     """
-    with open(folderName + fileName + ".pdf", 'wb') as f:
-        try:
-            f.write(requests.get(link).content)
-            print(fileName, "downloaded")
-        except requests.exceptions.ConnectionError:
-            print("HTTP Connection error:", link)
+    path = getUniquePath(folderName, preferred_file_name)
+    try:
+        content = requests.get(link).content
+        f = open(path, 'wb')
+        f.write(content)
+        f.close()
+        print(preferred_file_name, link)
+    except requests.exceptions.ConnectionError:
+        print("HTTP Connection error:", link)
+        downloadUsingPDFKit(link, path)
+    return path
+
+
+def downloadUsingPDFKit(link, path):
+    """
+        There was no reason to use selenium to download pdfs, instead I found this library called pdfkit
+        pdfkit will convert HTML pages to PDFs, some webpages they have HTML files instead of PDFs,
+        so pdfkit fixes the problem of converting those HTML pages to PDFs and downloading them
+
+        pdfkit depends on wkhtmltopdf
+        to install pdfkit: run "pip install pdfkit"
+        to install wkhtmltopdf: go to "https://wkhtmltopdf.org/downloads.html"
+    """
+    try:
+        pdfkit.from_url(link, path)
+    except Exception:
+        print("pdfkit was not able download,", link)
+
+if __name__ == "__main__":
+    # this is example of a webpage that this method doesn't work on
+    #downloadUsingPDFKit("https://www.4changeenergy.com/viewpdf.aspx/?Docs/efl_budsva12gad_o.pdf", "./PDFs/", "doesnt_work.pdf")
+    downloadUsingPDFKit("https://docs.championenergyservices.com/ExternalDocs?planname=PN2402&state=TX&language=EN", "./PDFs/doesnt_work.pdf")
+    # it works in all other cases
+    downloadUsingPDFKit("https://newpowertx.com/EmailHTML/efl.aspx?RateID=672&BrandID=5&PromoCodeID=446", "./PDFs/works.pdf")
