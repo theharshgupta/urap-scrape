df9f0e1799cab0880383b5e3a1ede9f1bcd6c760
Harsh Gupta <harsh@berkeley.edu>
2019-10-17 14:56:20 -0700

    email service - updated

---
+++ email_service.py
@@ -0,0 +1,31 @@
+import smtplib, ssl
+import json
+
+def send():
+    import smtplib
+    with open('credentials.json') as f:
+        credentials = json.load(f)
+        gmail_user = credentials['email']
+        gmail_password = credentials['password']
+
+    sent_from = gmail_user
+    to = ['harsh@berkeley.edu']
+    SUBJECT = 'Pythonic Email Bru'
+    TEXT = 'Hey, whaddup dawg?'
+
+    email_text = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+
+        print('EMAIL SENT')
+    except Exception as e:
+        print("Err, something went wrong \n ", e)
+
+
+
+send()
