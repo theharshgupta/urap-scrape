3b8fe801804a4170987b9a4a3b4cf275b0b3dcdf
Harsh Gupta <31355653+theharshgupta@users.noreply.github.com>
2020-02-28 02:53:00 -0800

    extracting the data from the CSV - column matching

--- texas/main.py
+++ texas/main.py
@@ -1,6 +1,7 @@
 import csv
 import pandas as pd
 from datetime import datetime
+import json
 
 
 class Plan:
@@ -8,7 +9,7 @@ class Plan:
     # Define class variables as per the column names
 
     supplier_name: int = None
-    rate_type: int = None
+    rate_type: str = None
     fixed_charge: bool = False
     variable_rate: int = None
     introductory_rate: int = None
@@ -28,14 +29,19 @@ class Plan:
     zipcode: str = None
     date_downloaded: str = str(datetime.today())
     tdu_service_territory: str = None
+    plan_name: str = None
+    variable_rate_500: float = 0.0
+    variable_rate_1000: float = 0.0
+    variable_rate_2000: float = 0.0
 
     def __init__(self, row_data):
         """
         Constructor for the class for the Plan
         :param row_data: type Python dictionary
         """
-        self.supplier_name = row_data['supplier_name']
-        self.rate_type = row_data['rate_type']
+        self.supplier_name = row_data['[RepCompany]']
+        self.plan_name = row_data['[Product]']
+        self.rate_type = row_data['RateType']
         self.fixed_charge = row_data['supplier_name']
         self.variable_rate = row_data['supplier_name']
         self.introductory_rate = row_data['introductory_rate']
@@ -52,7 +58,10 @@ class Plan:
         self.estimated_cost = row_data['estimated_cost']
         self.other_product_service = row_data['other_product_service']
         self.zipcode = row_data['zipcode']
-        self.tdu_service_territory = row_data['tdu_service_territory']
+        self.tdu_service_territory = row_data['[TduCompanyName]']
+        self.variable_rate_500 = row_data['[kwh500]']
+        self.variable_rate_1000 = row_data['[kwh1000]']
+        self.variable_rate_2000 = row_data['[kwh2000]']
 
 
 def parse_csv(filepath):
@@ -63,8 +72,13 @@ def parse_csv(filepath):
     """
 
     df = pd.read_csv(filepath)
-    data_dict = pd.DataFrame(df).to_dict()
-    print(data_dict)
+    headers = df.columns
+    data_list = pd.DataFrame(df).to_records()
+    data_dict = json.loads(pd.DataFrame(df).reset_index().to_json(orient='records'))
+    for d in data_dict:
+        print(d)
+
+
 
 if __name__ == '__main__':
     parse_csv("master_data.csv")
