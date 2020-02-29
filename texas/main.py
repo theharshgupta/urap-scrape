9104d8dbda833ad4ec46abd5a33b16cc4f11c09b
Harsh Gupta <harsh@berkeley.edu>
2020-02-29 04:37:24 -0800

    added both classes in same file

--- texas/main.py
+++ texas/main.py
@@ -1,9 +1,44 @@
 import csv
 import pandas as pd
 from datetime import datetime
+import requests
 import json
-from texas.api_call import API
-from datetime import datetime
+
+
+class API:
+    zipcodes = None
+    url = "http://api.powertochoose.org/api/PowerToChoose/plans?zip_code="
+    id_zipcode_map = {}
+
+    def __init__(self, zipcodes):
+        self.zipcodes = zipcodes
+        self.all_zips()
+
+    def api_data(self, zipcode):
+        """
+        Gets data and parses it as per PowerToChoose module specifications
+        :param zipcode: zipcode of the place
+        :return: None
+        """
+
+        response = requests.get(self.url + str(zipcode), verify=False)
+        # Each data row has an plan_id that should be same to the idKey in the CSV
+        data = json.loads(response.text)['data']
+        for row in data:
+            if row['plan_id'] in self.id_zipcode_map.keys():
+                self.id_zipcode_map[row['plan_id']] = self.id_zipcode_map[
+                                                          row['plan_id']] + [
+                                                          zipcode]
+            else:
+                self.id_zipcode_map[row['plan_id']] = [zipcode]
+
+    def all_zips(self):
+        """
+        creates API object for all the zipcodes
+        :return: None
+        """
+        for zipcode in self.zipcodes:
+            self.api_data(zipcode)
 
 
 class Plan:
@@ -90,7 +125,7 @@ def parse_csv(filepath):
 def map_zipcode():
     """
     This function will be mapping zipcodes to ids or some other identifier
-    for each of the plans in the input CSV
+    for each of the plans in the input CSV -
     :return: None
     """
     zipcodes = [75001, 75002, 71230]
