9597c63c08e4e6c2764e3a4389c9dd0dca5c73d6
Bryan Qiu <bbqiu@berkeley.edu>
2020-03-06 15:11:40 -0800

    start parsing

--- CT/CT_selenium.py
+++ CT/CT_selenium.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import bs4 as bs
-from bs4 import BeautifulSoup
-
 
 driver = webdriver.Chrome()
 driver.get("https://www.energizect.com/compare-energy-suppliers")  # get the page
@@ -37,12 +35,3 @@ with open("out.html","w") as out:
             1+1
 
 print(html)
-
-with open('out.html') as ex:
-    soup2 = BeautifulSoup(ex, 'html.parser')
-
-table = soup2.find_all('table', class_ = "nice_table responsive highlight_table display nowrap")
-table = table[0]
-for row in table.find_all('tr'):
-    for cell in row.find_all('td'):
-        print(cell.attrs)
