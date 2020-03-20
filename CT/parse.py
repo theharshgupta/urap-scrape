0ceeed8ab1639a2dffe515e465fe040aea0f4b21
Bryan Qiu <bbqiu@berkeley.edu>
2020-03-20 15:21:33 -0500

    finished csv

--- CT/parse.py
+++ CT/parse.py
@@ -1,79 +1,123 @@
+import csv
+from datetime import date
 import bs4 as bs
 
-with open('out.html') as ex:
-    soup = bs.BeautifulSoup(ex, 'html.parser')
+#get's the value of an attribute using a certain offset (described below)
+def getValue(string, sub, offset=2):
+    start = string.index(sub) + len(sub) + offset 
+    #+2 for ="
+    #0 if need id="plan
+    end = string.index("\"", start)
+    return string[start : end]
 
-table = soup.find_all('table', class_ = "nice_table responsive highlight_table display nowrap")
-table = table[0]
-data_list = []
-for row in table.find_all('tr'):
-    for cell in row.find_all('td'):
-        data_list.append(cell.attrs)
-        print(cell.attrs)
+#get's the number inside the content of a block with a certain attribute value
+def getNum(row, attribute, value):
+    content = str(row.find(attrs={attribute : value}).contents)
+    s = ''.join(x for x in content if x.isdigit()) #gets all numbers within the contents
+    if s:
+        return int(s)
+    else:
+        return 0
 
-#modify data_list into a 2d array so that each element corresponds to an entire row
-data_list_modified = [[]]
-for i in range(len(data_list)):
-    data_list_modified[i / 7][i % 7] =data_list[i]
+#fill suppliers with suppliers that have supplier information as instance variables
+def fill_suppliers():
+    table = soup.find_all('table', class_ = "nice_table responsive highlight_table display nowrap")[0]
+    first = True
+    planNum = 0
+    iterator = iter(table.find_all('tr'))
+    next(iterator) #skip first entry, which is a header
+    for row in iterator:
+        info = {}
+        rowString = str(row)
+        if row.attrs['style'] == "display: none;":
+            continue
+        info["TDU_service_territory"] = getValue(rowString, "data-ratetitle")
+        if first:
+            info["supplier_name"] = info["TDU_service_territory"]
+        else:
+            info["supplier_name"] = getValue(rowString, "data-friendly-name")
+        info["supplier_id"] = getValue(rowString, "id=\"plan-", 0)
+        info["incumbent_flag"] = first
+        info["plan_order_rank"] = planNum
+        mobilerateDiv = row.findAll("div", class_="mobilerate")[1]
+        contractTerm = ""
+        for elem in mobilerateDiv.find("div", class_="companyShortData").contents:
+            if "Billing Cycle" in elem:
+                contractTerm += elem.replace("\n", "").strip() + " "
+        info["contract_term"] = contractTerm
+        info["early_termination_fee"] = getNum(row, "id", "can_value")
+        info["enrollment_fee"] = getNum(row, "id", "enroll_value")
+        info["percent_renewable"] = getNum(row, "data-th", "RENEWABLE ENERGY")
+        info["rate_type"] = getValue(rowString, "data-priceplan")
+        info["variable_rate"] = '{:,.2f}¢'.format(getNum(row, "class", "supply_rate")/100)
+        info["fixed_charge"] = '${:,.2f}'.format(getNum(row, "id", "recur_value")/100)
+        info["additional_incentives"] = row.find("td", class_="col_7").contents[0].replace("\n", "").strip()
+        info["enroll_online"] = "Online Enrollment" in rowString
+        info["new_customer_only"] = "New Customer" in rowString
+        info["estimated_monthly_cost"] = '${:,.2f}'.format(getNum(row, "data-th", "GENERATION SUPPLY COST PER MONTH")/100)
+        if first:
+            info["estimated_savings"] = "$0.00"
+        else:
+            info["estimated_savings"] = "$" + getValue(rowString, "data-th=\"MONTHLY SAVINGS OR ADDITIONAL COST\"", 6)
+        first = False
+        planNum+=1
+        suppliers.append(Supplier(info))
 
-#parse all the available data so far and return an object with the data
-def parse_data(data):
-    for i in range(len(data)):
-        if data_list['class'] == ['col_0', 'compInfo']:
-            supplier_name = data_list['rel']
-        elif data_list['class'] == 'col_1':
-            variable_rate = data_list['rel']
-        elif data_list['class'] == 'col_2':
-            rate_type = data_list['rel']
-        elif data_list['class'] == 'col_3':
-            percent_renewable = data_list['rel']
-        elif data_list['class'] == 'col_4':
-            estimated_monthly_cost = data_list['rel']
-        elif data_list['class'] == 'col_5':
-            if 'rel' in data_list.keys():
-                estimated_savings = data_list['rel']
-            else:
-                break
-    #missing some data here
-    object = Data(TDU_service_territory, supplier_name,supplier_id, contract_term, early_termination_fee,
-                  enrollment_fee, percent_renewable, rate_type, variable_rate, fixed_charge, estimated_monthly_cost, estimated_savings)
-    return object
-for i in range(len(data_list_modified)):
-    parse_data(data_list_modified[i])
+def write_to_csv():
+    with open("TDU.csv", mode='w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(suppliers[0].info.keys())
+        for supplier in suppliers:
+            writer.writerow(supplier.info.values())
 
 
-class Data:
-    TDU_service_territory = None
-    supplier_name = None
-    supplier_id = None
-    contract_term = None
-    early_termination_fee = None
-    enrollment_fee = None
-    percent_renewable = None
-    rate_type = None
-    variable_rate = None
-    fixed_charge = None
-    enroll_online = None
-    date_downloaded = None
-    plan_order_rank = None
-    additional_incentives = None
-    enroll_online = None
-    new_customer_only = None
-    estimated_monthly_cost = None
-    estimated_savings = None
-    flag_incumbent = None
 
-    def __init__(self, TDU_service_territory, supplier_name, supplier_id, contract_term, early_termination_fee,
-                 enrollment_fee, percent_renewable, rate_type, variable_rate, fixed_charge,  estimated_monthly_cost, estimated_savings):
-        self.TDU_service_territory = TDU_service_territory
-        self.supplier_name = supplier_name
-        self.supplier_id = supplier_id
-        self.contract_term = contract_term
-        self.early_termination_fee = early_termination_fee
-        self.enrollment_fee = enrollment_fee
-        self.percent_renewable = percent_renewable
-        self.rate_type = rate_type
-        self.variable_rate = variable_rate
-        self.fixed_charge = fixed_charge
-        self.estimated_monthly_cost = estimated_monthly_cost
-        self.estimated_savings = self.estimated_savings
+#store all information as instance variables
+class Supplier:
+    date_downloaded = ""
+    TDU_service_territory = ""
+    supplier_name = ""
+    supplier_id = ""
+    incumbent_flag = False
+    plan_order_rank = -1
+    contract_term = -1 #in months
+    early_termination_fee = -1
+    enrollment_fee = -1
+    percent_renewable = -1 #percentage
+    rate_type = ""
+    variable_rate = "" #cost in C/kW
+    fixed_charge = "" #in $
+    additional_incentives = ""
+    enroll_online = False
+    new_customer_only = False
+    estimated_monthly_cost = -1 #for 750kW
+    estimated_savings = -1 #in comparison to incumbent
+    info = {}
+
+    def __init__(self, info):
+        self.date_downloaded = date.today();
+        self.TDU_service_territory = info["TDU_service_territory"]
+        self.supplier_name = info["supplier_name"]
+        self.supplier_id = info["supplier_id"]
+        self.incumbent_flag = info["incumbent_flag"]
+        self.plan_order_rank = info["plan_order_rank"]
+        self.contract_term = info["contract_term"]
+        self.early_termination_fee = info["early_termination_fee"]
+        self.enrollment_fee = info["enrollment_fee"]
+        self.percent_renewable = info["percent_renewable"]
+        self.rate_type = info["rate_type"]
+        self.variable_rate = info["variable_rate"]
+        self.fixed_charge = info["fixed_charge"]
+        self.additional_incentives = info["additional_incentives"]
+        self.enroll_online = info["enroll_online"]
+        self.new_customer_only = info["new_customer_only"]
+        self.estimated_monthly_cost = info["estimated_monthly_cost"]
+        self.estimated_savings = info["estimated_savings"]
+        self.info = info
+
+with open('out.html') as html:
+    soup = bs.BeautifulSoup(html, 'html.parser')
+
+suppliers = []
+fill_suppliers()
+write_to_csv()
