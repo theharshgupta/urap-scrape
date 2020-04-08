e52f319fd2c5507f4db1cef567a75e5314ceb120
mkleez <michael.li.gb@gmail.com>
2020-04-07 20:38:14 -0400

    PVD Update

--- CT/main.py
+++ CT/main.py
@@ -1,8 +1,10 @@
 import tdu_parse
 import tdu_scrape
+import Past_Variable_Data_Parse
 
 tdu_scrape.scrape('es')
 tdu_parse.run('es')
 
 tdu_scrape.scrape('ui')
 tdu_parse.run('ui')
+Past_Variable_Data_Parse()
