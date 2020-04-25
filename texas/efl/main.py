433ba1535cb55efd2f4c8380549e41edf96ede76
Harsh Gupta <hmgcapital@gmail.com>
2020-04-26 01:02:54 +0530

    working on pdf dataset

--- texas/efl/main.py
+++ texas/efl/main.py
@@ -11,8 +11,7 @@ os.chdir(dir_path)
 
 BUCKET_NAME = "zckharsh"
 PDF_FOLDER = pathlib.Path("/Users/harsh/Desktop/coding/urap-scrape/texas/PDFs/")
-
-
+dataset_planids = [12820,16606,12822,16612,16761,16690,16769,18359,18361,16759,16765,18423,18419,18421,12406,12411,18462,18465,18463,18464,17620,17621,16146,16141,17622,22032,22034,22029,22039,18735,18736,11561,11547,11502,11512,11513,11535,11536,11528,20854,20864,20866,20859,20861,20846,20848,4152,1883,1986,1879,1820,2859,1821,1987,22845,22120,22290,22419,22210,21834,22413,22647,5506,22624,6165,6168,5503,6163,5886,355,456,59,132,16822,16826,11119,11115,16823,16821,11118,4125,142,250,2023,1874,2027,66,4118,21197,22172,9628,16466,16442,16426,16469,22772,17319,17317,22771,20509,20516,20515,20505,18540,16539,16531,16509,16523,22783,22451,21694,22454,22455,21496,18374,17577,18370,11681,18373,18367,21855,21857,20724,22397,20090,22719,22708,22721,22703,22712,21869,21792,21636,21520,21790,20474,20472,19452,21934,20215,20200,20738,20744,20743,20735,21211,21213,22221,22732,22243,22512,22261,22254,22730,22744,22258,19845,22689,22687,21371,21124,19025,21370,21486,22544,22547,22013,22012,22018,21921,21329,12060,18860,21016,18779,18786,18774,18772,12292,22674,22909,17279,17301,18521,18537,18536,18520,20789,20805,20793,20798,20788,13322,13333,619,620,20850,20867,22445,22443,20178,20408,20373,21944,21943,21931,20670,20668,20673,20655,20678,20664,16771,20929,20927,20935,20904,20944,20920,19787,22890,21629,20959,22841,22237,22364,22825,15958,12885,13014,13017,13021,22411,22429,22431,22762,17257,17255,22761,22602,22575,22594,22586,22607,13151,13141,13140,13150,3200,3199,11372,3119,3061,18982,18954,246,18981,21147,19872,21344,20022,18865,12431,17234,18632,22167,22518,22748,15963,12973,12965,15954]
 def pdf_to_text(filepath):
     """
     Uses pdfplumber library to convert PDF to string.
@@ -124,16 +123,23 @@ def classify_pdf():
 
 def create_bucket():
     """Creates a new bucket."""
+    # lmao = loda.strip().replace('\n', ',')
+    # print(lmao)
+    # exit()
     for file in os.listdir("../PDFs"):
-        source_file_name = f"../PDFs/{file}"
-        destination_blob_name = f"PDF/{file}"
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(BUCKET_NAME)
-        blob = bucket.blob(destination_blob_name)
-        blob.upload_from_filename(source_file_name)
-        print(f"Uploaded at {destination_blob_name}")
+        strplan = file.strip().split('.pdf')[0]
+        print(int(strplan) in dataset_planids)
+        # print(file)
+        # source_file_name = f"../PDFs/{file}"
+        # destination_blob_name = f"PDF/{file}"
+        # storage_client = storage.Client()
+        # bucket = storage_client.bucket(BUCKET_NAME)
+        # blob = bucket.blob(destination_blob_name)
+        # blob.upload_from_filename(source_file_name)
+        # print(f"Uploaded at {destination_blob_name}")
 
 
 if __name__ == '__main__':
-    df_merge = stratify()
-    df_merge.to_csv("training_set_sample.csv")
+    # df_merge = stratify()
+    # df_merge.to_csv("training_set_sample.csv")
+    create_bucket()
