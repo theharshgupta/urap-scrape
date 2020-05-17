8b1320266e604d53012f637dc26bad769bdb1f00
JenyaKL <jenya.kahnlang@gmail.com>
2020-05-17 18:15:29 -0400

    Small edits get code working on my computer
    Also added some TX comments / deleted some unused code

--- CT/var_scrape.py
+++ CT/var_scrape.py
@@ -10,7 +10,8 @@ import bs4 as bs
 
 def scrape(supplier):
 
-    driver = webdriver.Chrome()
+    #driver = webdriver.Chrome()
+    driver = webdriver.Chrome(r'C:/Program Files/Chromedriver/chromedriver.exe')
     driver.get("https://www.energizect.com/compare-energy-suppliers")  # get the page
 
     if (supplier == "UI"):
@@ -23,8 +24,19 @@ def scrape(supplier):
     # Wait *up to* 10 seconds to make sure the page has finished loading (check that the button no longer exists)
     # WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.CLASS_NAME, "supplier_form_submit")))
 
-    #wait for the x to show up
-    WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.CLASS_NAME, "close_anchor")))
+    #TEMPERORARY FOR THE POPUP ABOUT STANDARD PRICES
+    try:
+        WebDriverWait(driver,20).until(EC.visibility_of_element_located((By.CLASS_NAME, "ui-dialog-titlebar-close")))
+        close_button = driver.find_element_by_class_name("ui-dialog-titlebar-close")
+        close_button.click()
+    except: 
+        print("no seasonal popup")
+
+    # Wait *up to* 20 seconds for the popup to show up 
+    try:
+        WebDriverWait(driver,20).until(EC.visibility_of_element_located((By.CLASS_NAME, "close_anchor")))
+    except: 
+        email_error.send_email("no close anchor")
 
     #click the x for a disclaimer
     close_button = driver.find_element_by_class_name("clostPopup")
