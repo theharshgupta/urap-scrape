54047d55a469c46aa28d72ecda37f91e418710aa
JenyaKL <jenya.kahnlang@gmail.com>
2020-05-20 00:36:16 -0400

    write dictionaries to CSV

--- test/jsonl_to_csv.py
+++ test/jsonl_to_csv.py
@@ -1,19 +1,13 @@
 import pandas as pd
 import json
 import csv
-with open('train1.jsonl', "r") as f:
-    records = [json.loads(line) for line in f]
-pd.DataFrame.from_records(records()).to_csv('testing.csv')
+
+#class Doc:
+#    info = {}
+#    def __init__(self, info):
+#        self.info = info
 
 
-class Doc:
-    info = {}
-
-    def __init__(self, info):
-        self.info = info
-
-
-#to be corrected
 def write_to_csv(doc):
     try:
         with open("testing.csv", mode = "w") as csv_file:
@@ -26,6 +20,15 @@ def write_to_csv(doc):
     except IOError:
         print("I/O error")
 
+def write_to_csv2(doc, keys):
+    try:
+        with open("testing2.csv", mode = "w", encoding='utf-8', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(keys)
+            for d in doc:
+                writer.writerow([d.get(i, "NA") for i in keys])
+    except IOError:
+        print("I/O error")
 
 def fill_suppliers(records, doc):
     for i in range(len(records)):
@@ -34,16 +37,25 @@ def fill_suppliers(records, doc):
         if 'annotations' in records[i].keys():
             for j in range(len(records[i]['annotations'])):
                 try:
-                    low =int(records[i]['annotations'][j]['textExtraction']['textSegment']['startOffset']) - 1
+                    low =int(records[i]['annotations'][j]['textExtraction']['textSegment']['startOffset'])
                     high = int(records[i]['annotations'][j]['textExtraction']['textSegment']['endOffset'])
                     info[records[i]['annotations'][j]['displayName']] = document[low:high]
                 except Exception:
                     1+1
-        doc.append(Doc(info))
+        doc.append(info)
 
+def get_fields(doc):
+    fields = set()
+    for i in range(len(doc)):
+        fields.update(doc[i].keys())
+    return fields
 
 def run():
+    with open('train1.jsonl', "r", encoding="utf8") as f:
+        records = [json.loads(line) for line in f]
     doc = []
     fill_suppliers(records, doc)
-    write_to_csv(doc)
+    fields = get_fields(doc)
+    write_to_csv2(doc, fields)
 
+run()
